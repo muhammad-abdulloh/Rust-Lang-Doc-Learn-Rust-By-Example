55c6630f84082d7baa8f669f07f5f4c0f07ae490
muhammad-abdulloh <clevercoderr@gmail.com>
2023-02-24 17:09:52 +0500

    Started Comments and Hello World First Section

---
+++ ex-one-helloworld/src/comments.rs
@@ -0,0 +1,15 @@
+fn main() {
+
+    // x ga 5 va 5 ni qo'shyabmiz
+    let x = 5 + 5;
+
+    /*
+        Ekranga chiqarilmoqda
+        x ning qiymatini menimcha formatlash orqali seniorcha uslibda
+        ekranga chiqarilish nazarda tutilgan
+     */
+    // formatlash uslubi C# bilan bir xil ekan 
+    // Welcome to Hello World 
+    
+    println!("Is `x` 10 or 100 x = {1}, y = {0}",x, x + 5);
+}
