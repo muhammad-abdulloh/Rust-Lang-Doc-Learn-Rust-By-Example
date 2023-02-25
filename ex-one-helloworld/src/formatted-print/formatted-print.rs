1689bce227c3f5b231e227e09736b95bc9edcd12
muhammad-abdulloh <clevercoderr@gmail.com>
2023-02-25 11:17:39 +0500

    open closed rust codes

--- ex-one-helloworld/src/formatted-print/formatted-print.rs
+++ ex-one-helloworld/src/formatted-print/formatted-print.rs
@@ -6,38 +6,38 @@
 // #![allow(unused_variables)]
 
 fn main(){
-/*
+
     let x = 5 + 6;
     let y = 8 + 8;
 
     println!("X = {1}, Y = {0}", x, y);
     print!("X = {}, Y = {}", x, y);
-*/
 
-/* 
+
+
     //formatlashning raqamli uslubi
     // {} => manashu qavsni qo'yib ichiga shunchaki raqam
     // yoki bo'sh qoldirilsa ham error beradurg'o'y
     println!("0, {0}, 2, {1}, 4, {1}, 5, {0}, {0}", "Salom", 456);
-*/
 
-/* 
+
+
     // Argumentlarni nomlash mumkin ekan 
     println!("{subject}, {verb}, {object}",
             object = "Bu object",
             subject = "Bu subject",
             verb = "BU verb");
-*/
 
-/*
+
+
     // sonlarni spesific tipga o'tkazish      
     println!("Base 10:                 {}", 69420);   // 69420
     println!("Base 2 (binary):         {:b}", 69420); // 10000111100101100
     println!("Base 8 (octal):          {:o}", 69420); // 207454
     println!("Base 16 (hexadecimal):   {:x}", 69420); // 10f2c
-*/
 
-/*
+
+
     // You can right-justify text with a specified width. This will
     // output "    1". (Four white spaces and a "1", for a total width of 5.)
     // docs da shunday deb yozilgan lekin nima uchun
@@ -56,9 +56,9 @@ fn main(){
 
     // bu holatda dastlab raqam yozilib qolgan joy o'ng tomonidan ajratilyabdi  
     println!("{number:<9}", number=12); // 12_______
-*/
 
-/*
+
+
     // biz bo'sh joylarni 0 bilan to'ldirishimiz mumkin ekan
     println!("{number:0>5}", number=1); // 00001
 
@@ -79,7 +79,8 @@ fn main(){
     // Rust hatto argumentlarning to'g'ri soniga ishonch hosil qilish uchun tekshiradi ekan qoyileee
     // hsuyerda 1 ni qo'yganimiz uchun error beradi chunki u argument yo'q mashnaqa :) -_- :)
     println!("My name is {0}, {1}, {0}", "Bond");
-*/
+
+
 
 // #[allow(dead_code)]
 // struct Structure(i32);
