f3e3d0182499d577788cb8ed28c1352e10fea33e
muhammad-abdulloh <clevercoderr@gmail.com>
2023-02-25 23:45:23 +0500

    learned debug for rust I chut chut understand ):

--- ex-one-helloworld/src/formatted-print/formatted-print-meta/debug.rs
+++ ex-one-helloworld/src/formatted-print/formatted-print-meta/debug.rs
@@ -1,3 +1,44 @@
+/**
+#[derive(Debug)]
+struct Structure(i32);
+
+#[derive(Debug)]
+struct Deep(Structure);
+
 fn main(){
-    println!("Tis is meta debug file");
+    // Printing with `{:?}` is similar to with `{}`.
+
+    println!("{:?} months in a year.", 12);
+
+    println!("{1:?} {0:?} is the {actor:?} name.", 
+    "Slater",
+    "Christian",
+    actor="actor's");
+
+    // `Structure` is printable!
+    println!("Now {:?} will print", Structure(3));
+
+    println!("Now {:?} will print!", Deep(Structure(7)));
+
+}
+*/
+
+#[derive(Debug)]
+struct Person<'a> {
+
+    // nimagadur mashi type lar bilan ishlayotganda
+    // tagchiziq qo'yib ishlash kerak ekan bo'masam warning berib diqqat qilyabdi
+    // nimo'sayam rust rust ekande
+    // menga esa warning va errorlar yoqmaydi haha :)
+    _name: &'a str,
+    _age: u8
+}
+
+fn main(){
+    let _name = "Peter";
+    let _age = 27;
+    let peter = Person { _name, _age };
+
+    // Pretty print :)
+    println!("{:?}", peter);  // Person { _name: "Peter", _age: 27}
 }
