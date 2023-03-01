853c17e60d2bc62819ed172eb463a71d315da791
muhammad-abdulloh <clevercoderr@gmail.com>
2023-03-01 22:11:26 +0500

    Learn Rust for Hobby yuuuuu :)))

---
+++ hobby-code/hobby.rs
@@ -0,0 +1,10 @@
+// let mut variable_name = value;
+// let mut variable_name:dataType = value;
+// Let us understand this with an example
+
+fn main() {
+   let mut fees:i32 = 25_000;
+   println!("fees is {} ",fees);
+   fees = 35_000;
+   println!("fees changed is {}",fees);
+}
